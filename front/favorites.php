82ebe80d243c6508e3728da10d3b0d445cce2d39
YannickMerleau <73471680+YannickMerleau@users.noreply.github.com>
2022-12-11 21:28:57 +0100

    fix bug

--- front/favorites.php
+++ front/favorites.php
@@ -3,6 +3,89 @@
     require_once 'source/head.php';
     require_once 'source/header.php';
     require_once '../public/functions.inc.php';
+    
+    if (isset($_GET['recipe'])) {//if we are in a detail page of recipe
+        ?>
+            <div class='recipe'>
+                <?php
+                foreach ($Recettes as $index_recipe => $recipes) { // foreach the list of recipe to find the one we want
+                    if ($recipes['titre'] == $_GET['recipe']) { // if we find the recipe we want
+                        $title = $recipes[array_keys($recipes)[0]]; // get the title of the recipe
+                        $ingredients = $recipes[array_keys($recipes)[1]]; // get the ingredients of the recipe
+                        $preparations = $recipes[array_keys($recipes)[2]]; // get the preparations of the recipe
+                        $index = $recipes[array_keys($recipes)[3]]; // get the index of the recipe
+                        $res =  get_index($title); // get the index of the recipe
+                        $ingredients_split = split_chain($ingredients, '|'); // split the ingredients in an array 
+                        $preparations_split = split_chain($preparations, '.'); // split the preparations in an array
+                        $name = valid_name($title); // get the name of the image
+        
+                        if (!file_exists("../public/photos/" . $name)) { // if the image doesn't exist we put a default image
+                            $name = 'cocktail.png';
+                        }
+                ?>
+                        <div class="in_detail_recipe">
+                            <h3> <?php echo $title; ?> </h3>
+                            <button class="button" id="<?php echo $res; ?>"> 
+                        <?php if (isset($_SESSION['favorites_temp']) || isset($favorites)){
+                            if(isset($_SESSION['favorites_temp'])){
+                                    $favorites_temp = $_SESSION['favorites_temp'];
+                                }else{
+                                    $favorites_temp = array();
+                                }
+                                if(isset($favorites)){
+                                    $favorite = $favorites;
+                                }else{
+                                    $favorite = array();
+                                }
+        
+                            if (in_array($res, $favorites_temp) || in_array($res, $favorite)) {
+                                ?><img src="../public/photos/heart_full.png" alt=""><?php
+                            } else {
+                                ?><img src="../public/photos/heart_empty.png" alt=""><?php
+                            }
+                        } else {
+                            ?><img src="../public/photos/heart_empty.png" alt=""><?php
+                        }?>
+                    </button>
+                            <br>
+        
+                            <img src="../public/photos/<?php echo $name; ?>" alt="" width="200">
+                            <br>
+                        </div>
+        
+                        <div class="detail_recipe">
+                            <h4>Liste d'ingrédient :</h4>
+                        </div>
+        
+                        <ul>
+                            <?php
+                            foreach ($ingredients_split as $key => $ingredient) {
+                            ?><li><?php echo $ingredient; ?></li><?php
+                                                                }
+                                                                    ?>
+                        </ul>
+        
+                        <br>
+        
+                        <div class="detail_recipe">
+                            <h4>Préparation :</h4>
+                        </div>
+        
+                        <ol>
+                            <?php
+                            foreach ($preparations_split as $key => $preparation) {
+                            ?><li><?php echo $preparation; ?></li><?php
+                                                                }
+                                                                    ?>
+                        </ol>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+            <?php
+        
+        } else {
 
     ?>
     <nav id='nav_index'>
@@ -29,8 +112,19 @@
                 <div class="card" style="width: 18rem;">
                     
                     <button class="button" id="<?php echo $res; ?>">
-                        <?php if (isset($_SESSION['favorites_temp'])){
-                            if (in_array($res, $_SESSION['favorites_temp'])) {
+                        <?php if (isset($_SESSION['favorites_temp']) || isset($favorites)){
+                    if(isset($_SESSION['favorites_temp'])){
+                            $favorites_temp = $_SESSION['favorites_temp'];
+                        }else{
+                            $favorites_temp = array();
+                        }
+                        if(isset($favorites)){
+                            $favorite = $favorites;
+                        }else{
+                            $favorite = array();
+                        }
+
+                    if (in_array($res, $favorites_temp) || in_array($res, $favorite)) {
                                 ?><img src="../public/photos/heart_full.png" alt=""><?php
                             } else {
                                 ?><img src="../public/photos/heart_empty.png" alt=""><?php
@@ -63,6 +157,7 @@
         }
         }
     }
+}
     ?>
     
     
--- public/users/admin.inc.php
+++ public/users/admin.inc.php
@@ -1,8 +1,8 @@
 <?php $user =array (
   'login' => 'admin',
   'password' => 'd033e22ae348aeb5660fc2140aec35850c4da997',
-  'name' => 'Doe',
-  'firstName' => 'John',
+  'name' => 'John',
+  'firstName' => 'Doe',
   'sex' => 'male',
   'birthDate' => '',
 )?>
